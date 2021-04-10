ef7c90af17887ad9ca3144b8f4219fd9c4ce309b
hyunjin <hyjilim@gmail.com>
2021-04-10 20:09:11 +0900

    [ React ]
    - 3.0 Class Components and State
    - 기존 App.js는 function component 였으나 이제 React.Component를 extends한 class component로 수정함
    - 이는 React.Component 클래스가 가변적인 데이터를 쓸 수 있게 해주는 state 오브젝트를 가지기 때문

--- src/App.js
+++ src/App.js
@@ -1,51 +1,29 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <p>App Component</p>
-      {foods.map(dish => {
-        return <Food key={dish.id} name={dish.name} price={dish.price} rating={dish.rating} /> ;
-      })}
-    </div>
-  );
-}
+class App extends React.Component {
+  
+  state = {
+    count: 0
+  }
+  
+  add = () => {
+    console.log("add");
+  }
 
-const foods = [{
-  id: 1,
-  name: "김밥",
-  price: 5000,
-  rating: 3.2
-},{
-  id: 2,
-  name: "국밥",
-  price: 4000,
-  rating: 1
-},{
-  id: 3,
-  name: "냉면",
-  price: 6000,
-  rating: 2.1
-},{
-  id: 4,
-  name: "돈까스",
-  price: 7000,
-  rating: 4.4
-}];
+  minus = () => {
+    console.log("minus");
+  }
 
-function Food({name, price, rating}){
-  return (
+  render(){
+    return (
     <div>
-      <h1>I like {name} and its price is {price}.({rating}/5)</h1>
-    </div>
-  )
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  rating: PropTypes.number.isRequired
+      <h1>The number is {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>)
+    ;
+  }
 }
 
 export default App;
