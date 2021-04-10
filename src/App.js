5f6dddd1b34b543bfbc1ec122e0865fe0c377241
hyunjin <hyjilim@gmail.com>
2021-04-10 19:22:10 +0900

    [ React ]
    - 2.3 map Recap 완료
    - map 함수를 더 잘 이해하게 되었고, react component가 내부적으로 각 component 구분을 위해 key 프로퍼티를 필요로 한다는 것을 알게 되었다.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
+import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
+
 function App() {
   return (
     <div className="App">
       <p>App Component</p>
-      {foods.map(food=>{
-        return <Food name={food.name} price={food.price} />;
+      {foods.map(dish => {
+        return <Food key={dish.id} name={dish.name} price={dish.price} /> ;
       })}
     </div>
   );
 }
 
 const foods = [{
+  id: 1,
   name: "김밥",
   price: 5000
 },{
+  id: 2,
   name: "국밥",
   price: 4000
 },{
+  id: 3,
   name: "냉면",
   price: 6000
 },{
+  id: 4,
   name: "돈까스",
   price: 7000
 }];
 
 function Food({name, price}){
   return (
-    <h1>I like {name} and its price is {price}.</h1>
+    <div>
+      <h1>I like {name} and its price is {price}.</h1>
+    </div>
   )
 }
 
