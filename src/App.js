f73069fe5707bce225a0a6a1db3efc4d365f415a
hyunjin <hyjilim@gmail.com>
2021-04-10 19:40:51 +0900

    [ React ]
    - npm i prop-types : father component로부터 전달받는 프로퍼티의 타입, 혹은 전달 유무 등을 알려줌
    - 반드시 {컴포넌트명}.propTypes 로 선언해야 react가 인식함

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
+import React from "react";
+import PropTypes from 'prop-types';
 
 function App() {
   return (
     <div className="App">
       <p>App Component</p>
       {foods.map(dish => {
-        return <Food key={dish.id} name={dish.name} price={dish.price} /> ;
+        return <Food key={dish.id} name={dish.name} price={dish.price} rating={dish.rating} /> ;
       })}
     </div>
   );
@@ -14,27 +15,37 @@ function App() {
 const foods = [{
   id: 1,
   name: "김밥",
-  price: 5000
+  price: 5000,
+  rating: 3.2
 },{
   id: 2,
   name: "국밥",
-  price: 4000
+  price: 4000,
+  rating: 1
 },{
   id: 3,
   name: "냉면",
-  price: 6000
+  price: 6000,
+  rating: 2.1
 },{
   id: 4,
   name: "돈까스",
-  price: 7000
+  price: 7000,
+  rating: 4.4
 }];
 
-function Food({name, price}){
+function Food({name, price, rating}){
   return (
     <div>
-      <h1>I like {name} and its price is {price}.</h1>
+      <h1>I like {name} and its price is {price}.({rating}/5)</h1>
     </div>
   )
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired
 }
 
 export default App;
